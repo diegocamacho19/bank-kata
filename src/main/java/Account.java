fba7386586698b390a0ca1431c0e2c2fe3447650
Diego Camacho <diegocsossa@gmail.com>
2022-05-03 16:12:15 -0500

    primer commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+
+    private Statement statement;
+    public Account(Statement statement){
+        this.statement = statement;
+    }
+
+    void deposit(int amount) {
+
+    }
+
+    void withdraw(int amount){
+
+    }
+
+    void printStatements() {
+
+    }
+
+}
---
+++ src/main/java/Statement.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Statement {
+
+    private Date fecha;
+    private Float monto;
+    private Float saldo;
+    public Statement(){
+
+    }
+
+}
