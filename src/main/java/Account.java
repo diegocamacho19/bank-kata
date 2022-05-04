e91cb27db1c1ed9850bbce2c8b1357be3241b322
Diego Camacho <diegocsossa@gmail.com>
2022-05-03 20:35:43 -0500

    avances

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,36 +1,38 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
 public class Account {
 
     private Statement statement;
+    private int count = 0;
 
-    private List<Statement> lstStatement;
+    private List<String> lstString;
     public Account(Statement statement){
         this.statement = statement;
-        lstStatement = new ArrayList<>();
+        lstString = new ArrayList<>();
     }
 
     void deposit(Float amount, Date fecha) {
         Float saldo = operation(amount, fecha);
         statement.setSaldo(saldo += amount);
-        lstStatement.add(statement);
+        lstString.add(statement.getFecha()+"  "+statement.getMonto()+"  "+ statement.getRetiro()+ "  "+statement.getSaldo());
 
     }
 
     void withdraw(Float amount, Date fecha){
         Float saldo = operation(amount, fecha);
         statement.setSaldo(saldo -= amount);
+        statement.setMonto(Float.valueOf(0));
         statement.setRetiro(amount);
-        lstStatement.add(statement);
-
+        lstString.add(statement.getFecha()+"  "+statement.getMonto()+"  "+ statement.getRetiro()+ "  "+statement.getSaldo());
     }
 
     void printStatements() {
         System.out.println("date  credit  debit  balance");
-        lstStatement.stream().forEach(statement ->{
-            System.out.println(statement.getFecha()+"  "+statement.getMonto()+"  "+ statement.getRetiro()+ "  "+statement.getSaldo());
+        lstString.stream().sorted(Comparator.reverseOrder()).forEach(statementString ->{
+            System.out.println(statementString);
         });
     }
 
